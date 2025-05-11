fee0d5d794ab22818bac7f73840908746c906fe1
Shantanu <deltaairways_hill@simplelogin.com>
2025-05-11 21:56:05 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <div className={styles.ctas}>
+          Hello
+        </div>
+      </main>
+    </div>
+  );
+}
